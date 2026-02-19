743d578afec33decba84465f3ad832e97a192568
komatsu kana <kirakiraipai70@gmail.com>
2026-02-19 13:47:11 +0900

    ヘッダー仮完成

---
+++ src/views/header.php
@@ -0,0 +1,21 @@
+<header class="site-header">
+
+  <h1 class="header-title">小松加奈の不安ブログ</h1>
+
+  <p class="header-sub">どんな気持ちも優しく包み込む</p>
+
+  <ul class="header-nav">
+
+    <li class="header-nav-item">
+      <a href="/about.php">小松加奈とは</a>
+    </li>
+
+    <li class="header-nav-item">
+      <a href="/post.php">ブログ</a>
+    </li>
+
+  </ul>
+
+</header>
+
+
